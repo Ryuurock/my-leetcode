de8bbc4f87bfba0733a5b89cfcceee91fe1a3d84
longwanxiang <wanxiang.long@wukongshc.com>
2021-11-17 15:50:49 +0800

    两数之和

--- src/two-sum.ts
+++ src/two-sum.ts
@@ -1,13 +1,39 @@
+/**
+ * 这真是太妙了
+ * @param nums
+ * @param target
+ * @returns
+ */
 export const twoSum = (nums: number[], target: number) => {
   const map = new Map();
 
-  nums.forEach((num, index) => {
+  let returns: number[] = [];
+
+  nums.some((num, index) => {
     const complement = target - num;
     if (map.has(complement)) {
-      return [map.get(complement), index];
+      returns = [map.get(complement), index];
+      return true;
     }
     map.set(num, index);
   });
 
-  return [];
+  return returns;
+};
+
+/**
+ * 我只会这么写 还是copilot写的
+ *
+ * @param nums
+ * @param target
+ * @returns
+ */
+export const twoSumMine = (nums: number[], target: number) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
 };
