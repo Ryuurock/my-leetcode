31f7678857493498d6d0152cc0ff9f79909bc7bd
longwanxiang <wanxiang.long@wukongshc.com>
2022-01-04 16:40:55 +0800

    三数之和

--- 15.三数之和.ts
+++ 15.三数之和.ts
@@ -7,45 +7,43 @@
 // @lc code=start
 function threeSum(nums: number[]): number[][] {
   nums.sort();
+
+  if (nums.length < 3) {
+    return [];
+  }
+
   const ans: number[][] = [];
-  const numsLength = nums.length;
-  for (let firstIndex = 0; firstIndex < numsLength; ++firstIndex) {
-    if (firstIndex > 0 && nums[firstIndex] === nums[firstIndex - 1]) {
+  const n = nums.length;
+
+  for (let i = 0; i < n; i++) {
+    const first = nums[i];
+    let left = i + 1,
+      right = n - 1;
+    if (first > 0) {
+      return ans;
+    }
+
+    if (i > 0 && first === nums[i - 1]) {
       continue;
     }
 
-    // 题目要求 first + second + third = 0
-    // 那么 -first = second + third
-    // 所以这里是负数
-    const target = -nums[firstIndex];
-    let thirdIndex = nums.length - 1;
+    while (right > left) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (sum === 0) {
+        ans.push([nums[i], nums[left], nums[right]]);
+        while (right > left && nums[left] === nums[left + 1]) {
+          left++;
+        }
+        while (right > left && nums[right] === nums[right - 1]) {
+          right--;
+        }
 
-    for (
-      let secondIndex = firstIndex + 1;
-      secondIndex < numsLength;
-      ++secondIndex
-    ) {
-      if (
-        secondIndex > firstIndex + 1 &&
-        nums[secondIndex] === nums[secondIndex - 1]
-      ) {
-        continue;
-      }
-
-      while (
-        secondIndex < thirdIndex &&
-        /* 这里随着thirdIndex的左移，和second的和会越来越小并接近target */
-        nums[secondIndex] + nums[thirdIndex] > target
-      ) {
-        --thirdIndex;
-      }
-
-      if (secondIndex === thirdIndex) {
-        break;
-      }
-
-      if (nums[secondIndex] + nums[thirdIndex] === target) {
-        ans.push([nums[firstIndex], nums[secondIndex], nums[thirdIndex]]);
+        left++;
+        right--;
+      } else if (sum > 0) {
+        right--;
+      } else {
+        left++;
       }
     }
   }
@@ -55,14 +53,7 @@ function threeSum(nums: number[]): number[][] {
 // @lc code=end
 
 test("threeSum", () => {
-  expect(threeSum([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4])).toEqual([
-    [-4, 0, 4],
-    [-4, 1, 3],
-    [-3, -1, 4],
-    [-3, 0, 3],
-    [-3, 1, 2],
-    [-2, -1, 3],
-    [-2, 0, 2],
+  expect(threeSum([-1, 0, 1, 2, -1, -4])).toEqual([
     [-1, -1, 2],
     [-1, 0, 1],
   ]);
