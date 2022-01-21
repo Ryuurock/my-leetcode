47507b15f1bf4f3d414bbe421bfe903c51950e2c
longwanxiang <wanxiang.long@wukongshc.com>
2022-01-21 10:59:45 +0800

    二叉树的最大深度

--- global.d.ts
+++ global.d.ts
@@ -1,8 +1,12 @@
 declare class ListNode {
   val: number;
   next: ListNode | null;
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-  }
+  constructor(val?: number, next?: ListNode | null);
+}
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null);
 }
